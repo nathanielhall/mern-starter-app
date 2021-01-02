50990f85d5cdb73855b5314b27414eb9c2d29900
Nathaniel Hall <nathaniel.hall@gmail.com>
2021-01-02 07:26:54 -0500

    backend setup (todos example)

---
+++ server/models/todo.model.js
@@ -0,0 +1,13 @@
+const mongoose = require('mongoose')
+const Schema = mongoose.Schema
+
+const TodoSchema = new Schema({
+  action: {
+    type: String,
+    required: [true, 'The todo textfield is required']
+  }
+})
+
+const Todo = mongoose.model('todo', TodoSchema)
+
+module.exports = Todo
