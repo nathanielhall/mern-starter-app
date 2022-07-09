050d5b760c2d83c9b742a71938dc0d3130a38f48
Nathaniel C. Hall <nathaniel.hall@gmail.com>
2022-07-09 19:20:31 -0400

    prisma setup \ auth dtos

--- client/src/application.tsx
+++ client/src/application.tsx
@@ -1,19 +1,57 @@
+// import React, {FC} from 'react'
+// import { QueryClient, QueryClientProvider, useQuery } from 'react-query'
+// const queryClient = new QueryClient()
+
+export const Application = () => {
+
+  return(
+    <span>Sigin Page</span>
+  )
+}
 
 // type Todo = {
 //   _id: number
 //   action: string
 // }
 
-export const Application = () => {
-  // TODO: Replace with react query
-  // const [request, response] = useApi<Todo>('http://localhost:5000/signin')
+// export const Application = () => {
+//   // TODO: Replace with react query
+//   // const [request, response] = useApi<Todo>('')
 
-  // return (
-  //   <div>
-  //     {request.loading === false && !!response?.data && (
-  //       <pre>{JSON.stringify(response.data, null, 2)}</pre>
-  //     )}
-  //   </div>
-  // )
-  return (<span>Hello World</span>)
-}
+//   return (
+//     <QueryClientProvider client={queryClient}>
+//       <Example />
+//     </QueryClientProvider>
+//   )
+// }
+
+// const Example = () => {
+//   const { isLoading, error, data } = useQuery(['repoData'], () =>
+//     fetch('http://localhost:5000/').then((res) => res.json())
+//   )
+
+//   if (isLoading) return <Loading />
+
+//   if (error) return <ErrorMessage error={error} />
+
+//   return (
+//     <div>
+//       {isLoading === false && <pre>{JSON.stringify(data, null, 2)}</pre>}
+//     </div>
+//   )
+// }
+
+// const Loading = () => {
+//   return (
+//     <span>{`Loading`}</span>
+//   )
+// }
+
+// type ErrorMessageProps = {
+//   error?: {message: string} 
+// }
+// const ErrorMessage: FC<ErrorMessageProps> = ({error}) => {
+//   return (
+//     <span>{`Error: ${error?.message}`}</span>
+//   )
+// }
