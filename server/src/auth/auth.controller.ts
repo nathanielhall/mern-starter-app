cb2d6807dfdae6dd6102eff43f9b9ffd8b6c54be
Nathaniel C. Hall <nathaniel.hall@gmail.com>
2022-07-10 06:23:24 -0400

    signin / signup functional (need to add http error codes)

--- server/src/auth/auth.controller.ts
+++ server/src/auth/auth.controller.ts
@@ -1,17 +1,18 @@
-import { Controller, Post } from '@nestjs/common';
+import { Body, Controller, Post } from '@nestjs/common';
 import { AuthService } from './auth.service';
+import { AuthDto } from './dto';
 
 @Controller('auth')
 export class AuthController {
   constructor(private authService: AuthService) {}
 
   @Post('signin')
-  signin() {
-    this.authService.signin();
+  signin(@Body() dto: AuthDto) {
+    return this.authService.signin(dto);
   }
 
   @Post('signup')
-  signup() {
-    this.authService.signup();
+  signup(@Body() dto: AuthDto) {
+    return this.authService.signup(dto);
   }
 }
--- server/src/auth/auth.service.ts
+++ server/src/auth/auth.service.ts
@@ -1,13 +1,40 @@
-import { Injectable } from '@nestjs/common';
+import { ForbiddenException, Injectable } from '@nestjs/common';
 import { PrismaService } from 'src/prisma/prisma.service';
+import { AuthDto } from './dto';
+import * as argon from 'argon2';
 
 @Injectable()
 export class AuthService {
   constructor(private prisma: PrismaService) {}
-  signin() {
-    return 'I have signed in';
+
+  async signup(dto: AuthDto) {
+    console.log('dto', dto);
+
+    const hash = await argon.hash(dto.password);
+
+    const user = await this.prisma.user.create({
+      data: { email: dto.email, hash }
+    });
+
+    delete user.hash;
+
+    // return this.signToken(user.id, user.email);
+    return user;
   }
-  signup() {
-    return 'I have signed up';
+
+  async signin(dto: AuthDto) {
+    const user = await this.prisma.user.findUnique({
+      where: { email: dto.email }
+    });
+
+    if (!user) throw new ForbiddenException('Incorrect Credentials');
+
+    const verifyCredentials = await argon.verify(user.hash, dto.password);
+
+    if (!verifyCredentials)
+      throw new ForbiddenException('Incorrect Credentials');
+
+    // return this.signToken()
+    return user;
   }
 }
